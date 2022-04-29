ee8308105bb80a70e56cf8b114688801014653cc
yrvschneider <yrvschneider@gmail.com>
2022-04-28 22:01:55 -0300

    Subindo exercicios

---
+++ GustavoGuanabara/Mundo-2-EstruturasDeControle/Exercicios/53-DetectorDePalindromo.py
@@ -0,0 +1,13 @@
+# Crie um programa que leia uma frase qualquer
+#  e diga se ela é um palíndromo, desconsiderando
+#  os espaços.
+
+# Palíndromo é uma frase que pode ler de frente
+#  para trás e de trás para frente, dando a mesma
+#  coisa na leitura.
+
+# Exemplos
+# Apos a Sopa
+# A sacada da casa
+# O lobo ama o bolo
+# Anotaram a data da maratona
